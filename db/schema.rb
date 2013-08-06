3cee3f78e1f9f267ae1aa2b10abf54d286332d11
Guimaraes Mahota <guimaraesmahota@gmail.com>
2013-08-07 00:51:39 +0200

    add table user

--- db/schema.rb
+++ db/schema.rb
@@ -82,4 +82,15 @@ ActiveRecord::Schema.define(:version => 20130731231945) do
     t.datetime "updated_at",    :null => false
   end
 
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.string   "username"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
+  end
+
 end
