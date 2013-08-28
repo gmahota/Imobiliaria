32c008ca5eb18ef3604f48a9304f1af40c85479e
Guimaraes Mahota <guimaraesmahota@gmail.com>
2013-08-29 01:49:19 +0200

    add on reports picture and google maps pic

--- app/models/imovel.rb
+++ app/models/imovel.rb
@@ -22,12 +22,11 @@ class Imovel < ActiveRecord::Base
       @imovel = self.where("tipoNegocio LIKE ? and tipoImovel LIKE ? and  cidade LIKE ? and tipologia LIKE?" , "%#{idNegocio}%", "%#{idtipoImovel}%" , "%#{idCidade}%", "%#{idTipologia}%")
     
       
-      #if precoMin.blank? and precoMax.blank?
+      if precoMin.blank? and precoMax.blank?
         #@imovel = self.where("(preco BETWEEN ? and ?) and tipoNegocio LIKE ? and tipoImovel LIKE ? and  cidade LIKE ? and tipologia LIKE?", "%#{precoMin}%", "%#{ precoMax}%" , "%#{idNegocio}%", "%#{idtipoImovel}%" , "%#{idCidade}%", "%#{idTipologia}%")
-      #else
-      #  @imovel = self.where("tipoNegocio LIKE ? and tipoImovel LIKE ? and  cidade LIKE ? and tipologia LIKE?", "%#{idNegocio}%", "%#{idtipoImovel}%" , "%#{idCidade}%", "%#{idTipologia}%")
-      
-      #end
+      else
+        @imovel = self.where("tipoNegocio LIKE ? and tipoImovel LIKE ? and  cidade LIKE ? and tipologia LIKE?", "%#{idNegocio}%", "%#{idtipoImovel}%" , "%#{idCidade}%", "%#{idTipologia}%")
+      end
       
     else
       
