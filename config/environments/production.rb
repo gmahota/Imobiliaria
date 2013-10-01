4105b8eeea829a5cf4222a0ecc8075be2993db76
gmahota <guimaraesmahota@gmail.com>
2013-10-01 20:46:11 +0200

    gestao de layout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,5 @@
 MicroApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  
-  #para as imagens ficarem fixas
-  config.assets.initialize_on_precompile = false
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -12,13 +9,13 @@ MicroApp::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -27,7 +24,7 @@ MicroApp::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
-   config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
