9a3f0726211066aabf1d199bad9f907a402be93a
Guimaraes <guimaraesmahota@gmail.com>
2013-11-25 19:56:52 +0200

    adicionar localizacao nas pesquizas

--- app/models/imovel.rb
+++ app/models/imovel.rb
@@ -7,7 +7,7 @@ class Imovel < ActiveRecord::Base
   
   self.per_page = 3
   
-  def self.search(idreferencia , idNegocio , idtipoImovel , idTipologia , idCidade, precoMin , precoMax)
+  def self.search(idreferencia , idNegocio , idtipoImovel , idTipologia , idCidade, idlocalizacao, precoMin , precoMax)
     @imovel
     if idreferencia.blank?
      @imovel = self.where(:tipoNegocio => idNegocio) unless idNegocio.blank?
@@ -15,14 +15,14 @@ class Imovel < ActiveRecord::Base
      @imovel = self.where(:cidade => idNegocio) unless idCidade.blank?
      @imovel = self.where(:tipologia => idtipoImovel) unless idTipologia.blank?
      @imovel = self.where(preco: precoMin..precoMax) unless ( precoMin.blank? and precoMax.blank? )  
-     
+     @imovel = self.where(:localizacao => idlocalizacao) unless idlocalizacao.blank?
       
     else
       
       @imovel = self.where(:id => idreferencia)
       
     end
-    if idreferencia.blank? and idNegocio.blank? and idtipoImovel.blank? and idTipologia.blank? and idCidade.blank? and precoMin.blank? and precoMax.blank?
+    if idreferencia.blank? and idlocalizacao.blank? and idNegocio.blank? and idtipoImovel.blank? and idTipologia.blank? and idCidade.blank? and precoMin.blank? and precoMax.blank?
       @imovel = self.where(id: 0..1000000000);
     end
     @imovel.order('id desc')
